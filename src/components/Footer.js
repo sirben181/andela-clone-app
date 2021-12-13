7bb94f02f6e81923a742a7887c6b8b145b06c235
sirben181 <bensonkamauben@gmail.com>
2021-12-13 21:59:09 +0300

    design almost complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,25 +12,27 @@ const Footer = () => {
             </div>
             <div className="expertise_footer">
                 <h4>Andela has experts in:</h4>
-                <div>
+                <div className="skill_blocks">
+                <div className="skills_block1">
                     <p>Angular</p>
                     <p>Data Engineering</p>
                     <p>DevOps</p>
                 </div>
-                <div>
+                <div className="skills_block2">
                     <p>Golang</p>
                     <p>iOS</p>
                     <p>Java</p>
                 </div>
-                <div>
+                <div className="skills_block3">
                     <p>Python</p>
                     <p>QA</p>
                     <p>React Native</p>
                 </div>
-                <div>
+                <div className="skills_block4">
                     <p>React.js</p>
                     <p>Ruby</p>
                     <p>Salesforce</p>
+                </div>
                 </div>
             </div>
             <div className="main_footer">
@@ -38,29 +40,34 @@ const Footer = () => {
                     <img style={{height:'40px'}}src="https://andela.com/wp-content/uploads/2021/06/Andela_PrimaryLogo_TM_2021_AndelaBlue.svg"alt=""/>
                     <p>Connecting brilliance with opportunity</p>
                     <div className="socialmedia_icons">
-                        <FaFacebookSquare />
-                        <FaLinkedin />
-                        <FaGithub />
-                        <FaTwitterSquare />
-                        <FaInstagramSquare />
+                        <FaFacebookSquare style={{marginRight:'20px',fontSize:'40px',color:'blue'}}/>
+                        <FaLinkedin  style={{marginRight:'20px',fontSize:'40px',color:'blue'}}/>
+                        <FaGithub style={{marginRight:'20px',fontSize:'40px'}}/>
+                        <FaTwitterSquare style={{marginRight:'20px',fontSize:'40px',color:'blue'}}/>
+                        <FaInstagramSquare style={{marginRight:'20px',fontSize:'40px',color:'purple'}}/>
                     </div>
                 </div>
-                <div className='hire-block'>
-                    <p>For companies</p>
-                    <p>Enterprise</p>
-                </div>
-                <div className="joining-andela">
-                <p>For Engineers</p>
-                  <p>Learning Community</p>
-                    <p>Technical Leadership Program (Rwanda)</p>
-                </div>
-                <div className='about_us'>
-                    <a>Resources</a><br/>
-                    <a>Events</a><br/>
-                    <a>Blog</a><br/>
-                    <a>Press</a><br/>
-                    <a>About Andela</a><br/>
-                    <a>Career</a><br/>
+                <div className="hire_block_container">
+                    <div className='hire-block'>
+                    <h3>Hire talent</h3>
+                        <p>For companies</p>
+                        <p>Enterprise</p>
+                    </div>
+                    <div className="joining-andela">
+                        <h3>Join Andela</h3>
+                    <p>For Engineers</p>
+                        <p>Learning Community</p>
+                        <p>Technical Leadership Program (Rwanda)</p>
+                    </div>
+                    <div className='about_us'>
+                        <h3>About us</h3>
+                        <p>Resources</p><br/>
+                        <p>Events</p><br/>
+                        <p>Blog</p><br/>
+                        <p>Press</p><br/>
+                        <p>About Andela</p><br/>
+                        <p>Career</p><br/>
+                    </div>
                 </div>
             </div>
             <div className="actual_footer">
