bc491533f4591c3b815246cdf8afc71652619010
sirben181 <bensonkamauben@gmail.com>
2022-02-03 21:37:41 +0300

    improved

--- src/components/Leader_message.js
+++ src/components/Leader_message.js
@@ -4,7 +4,10 @@ const Leader_message = () => {
     return (
         <div className='leader_message_container'>
             <div className="wrapper_message">
-                <h3>Sales Rabbit</h3>
+                <div className='logo-testimonial'>
+                <img src='https://andela.com/wp-content/uploads/2021/01/img_testimonial.png' height='400px'/>
+                    
+                </div>
             <div className="themessage">
                 <p class="l_message">“Every one of the Andela engineers I have worked with has been stellar. <br/>They are great engineers, really driven to deliver, and they’re just great people.”</p>
                 <p className="authorname">Michael Archibald</p>
